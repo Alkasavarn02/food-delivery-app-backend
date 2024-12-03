c8f622815dd629901a70729ef32996260eb0b861
Alka savarn <alkasavarn02@gmail.com>
2024-12-03 21:36:19 +0530

    created food delivery backend application

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const fileUpload = require("express-fileupload");
+
+const cors = require("cors");
+const dbConnect = require("./Dbconfig/dbconfig");
+const cloudinaryConnect = require("./cloudinaryconfig/cloudinaryconfig");
+const userRouter = require("./Routes/UserRoutes");
+const restaurantRouter = require("./Routes/restaurantRoutes");
+const productRouter = require("./Routes/productRoutes")
+
+const PORT = process.env.PORT || 4000;
+
+app.use(cors({
+    origin: ['https://pro-manage-frontend-beta.vercel.app', 'http://localhost:5173'],
+    credentials: true           
+}));
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(fileUpload(
+    {
+        useTempFiles:true,
+        tempFileDir:"/tmp/"
+    }
+))
+
+app.use('/api/v1',userRouter)
+app.use('/api/v1',restaurantRouter)
+app.use("/api/V1",productRouter)
+
+app.listen(PORT,()=>{
+    console.log(`Server started at Port ${process.env.PORT}`)
+})
+
+dbConnect();
+cloudinaryConnect();
+
+
+
+
