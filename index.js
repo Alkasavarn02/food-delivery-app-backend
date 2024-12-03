491de91092b898c793e75f0104ce5b17516f204e
Alka savarn <alkasavarn02@gmail.com>
2024-12-03 22:43:44 +0530

    created food delivery backend application

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const productRouter = require("./Routes/productRoutes")
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-    origin: ['https://food-delivery-app-frontend-delta.vercel.app/', 'http://localhost:5173'],
+    origin: ['https://food-delivery-app-frontend-delta.vercel.app', 'http://localhost:5173'],
     credentials: true           
 }));
 
